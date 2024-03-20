c274a8d5c7b46741f20b2d8e1d62f1d11d43c46d
PaolaBasualdo <paoladeembalse@gmail.com>
2024-03-20 19:18:18 -0300

    Agregar ejercicio HTML y CSS del 1 al 40

--- JS-Del_41/42.js
+++ JS-Del_41/42.js
@@ -1,4 +1,5 @@
 /*42. Define una variable llamada `edad` y asígnale tu edad. Luego, muestra un mensaje que diga tienes "X" anos en la consola remplazando x con el valor de la variable"
 */
 let edad= 49;
-console.log("Tienes", edad, "anos");
+//console.log("Tienes", edad, "anos");
+console.log(`Tienes ${edad} ano`);
